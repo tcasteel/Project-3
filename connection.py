67cd0f9acf953ea108a0c43e936cb8692038fc35
CaseyScow <caseyscow@gmail.com>
2024-03-04 12:15:39 -0600

    Pushing HTML and flask connection with charts

--- connection.py
+++ connection.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import psycopg2
+from flask import request
 
 app = Flask(__name__)
 
@@ -10,28 +11,36 @@ password = "postgres"
 host = "localhost"  
 port = "5432"       
 
-@app.route('/')
+@app.route('/', methods =['GET','POST'])
 def index():
-    # Connect to the database
-    conn = psycopg2.connect(
-        dbname=dbname,
-        user=user,
-        password=password,
-        host=host,
-        port=port
-    )
+    if request.method == 'POST':
+        selected_year = request.form['year']
+        selected_industry = request.form['industry']  
+
+        # Connect to the database
+        conn = psycopg2.connect(
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+        )
     
-   # Execute a sample SQL query
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM industries_db")
-    rows = cursor.fetchall()
+        # Construct dynamic SQL query
+        query = "SELECT * FROM industries_db WHERE year = %s AND industry = %s"
 
+        # Execute query with parameters
+        cursor = conn.cursor()
+        cursor.execute(query, (selected_year, selected_industry))
+        rows = cursor.fetchall()
 
+        # Close cursor and connection
+        cursor.close()
+        conn.close()
 
-    # Close cursor and connection
-    cursor.close()
-    conn.close()
-    # Pass the data to the template
-    return render_template('idea.html', rows=rows)
+        # Pass the data to the template
+        return render_template('idea.html', rows=rows)
+    return render_template('idea.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
